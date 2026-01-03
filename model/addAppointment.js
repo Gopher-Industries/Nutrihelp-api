275c91be4e28cb70baf3a793888857c63fe85e0c
Chris Wen <chriswen430@gmail.com>
2026-01-04 01:00:12 +1100

    appointment api update

--- model/addAppointment.js
+++ model/addAppointment.js
@@ -11,4 +11,44 @@ async function addAppointment(userId, date, time, description) {
     }
 }
 
-module.exports = addAppointment;
+async function addAppointmentV2({
+  userId,
+  title,
+  doctor,
+  type,
+  date,
+  time,
+  location,
+  address,
+  phone,
+  notes,
+  reminder
+}) {
+  try {
+    const { data, error } = await supabase
+      .from("appointments")
+      .insert({
+        user_id: userId,
+        title,
+        doctor,
+        type,
+        date,
+        time,
+        location,
+        address,
+        phone,
+        notes,
+        reminder
+      })
+      .select()
+      .single();
+
+    if (error) throw error;
+    return data;
+  } catch (err) {
+    throw err;
+  }
+}
+
+
+module.exports = {addAppointment,addAppointmentV2};
