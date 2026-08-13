016665a2f707e4eecd7bccda0e4e3144f0926183
Ishaan <74845759+iamrealishaan@users.noreply.github.com>
2026-08-13 21:02:13 +1000

    Cs 5 xss sanitisation middleware (#281)
    * middleware funciton
    
    * added middleware to santize input and added that to recpieReview route
    
    * pushing middleware to sanitize input and applied to appointment and recpieRiview routes
    
    * added sanitize input to contactus.js, recpie, signup and userprofile routes
    
    * error logging

--- routes/recipeReviews.js
+++ routes/recipeReviews.js
@@ -1,13 +1,27 @@
+// const express = require('express');
+// const router = express.Router();
+// const recipeReviewController = require('../controller/recipeReviewController');
+// const { authenticateToken } = require('../middleware/authenticateToken');
+// const authorizeRoles = require('../middleware/authorizeRoles');
+
+// router.get('/feed', recipeReviewController.listFeed);
+// router.get('/', recipeReviewController.listReviews);
+// router.post('/summary', recipeReviewController.getSummaries);
+// router.post('/', authenticateToken, recipeReviewController.createReview);
+// router.delete('/:id', authenticateToken, authorizeRoles('admin'), recipeReviewController.hideReview);
+
+// module.exports = router;
 const express = require('express');
 const router = express.Router();
 const recipeReviewController = require('../controller/recipeReviewController');
 const { authenticateToken } = require('../middleware/authenticateToken');
 const authorizeRoles = require('../middleware/authorizeRoles');
+const sanitizeInput = require('../middleware/sanitizeInput');
 
 router.get('/feed', recipeReviewController.listFeed);
 router.get('/', recipeReviewController.listReviews);
 router.post('/summary', recipeReviewController.getSummaries);
-router.post('/', authenticateToken, recipeReviewController.createReview);
+router.post('/', authenticateToken, sanitizeInput, recipeReviewController.createReview);
 router.delete('/:id', authenticateToken, authorizeRoles('admin'), recipeReviewController.hideReview);
 
 module.exports = router;
--- routes/userprofile.js
+++ routes/userprofile.js
@@ -1,9 +1,38 @@
+// const express = require("express");
+// const router = express.Router();
+// const { authAndIdentity } = require('../controller');
+// const { authenticateToken } = require('../middleware/authenticateToken');
+// const authorizeRoles = require('../middleware/authorizeRoles');
+// const validate = require('../middleware/validateRequest');
+// const { updateUserProfileValidation } = require('../validators/userProfileValidator');
+
+// const {
+//   userProfile: controller,
+//   updateUserProfile: updateUserProfileController
+// } = authAndIdentity;
+
+// router.get('/', authenticateToken, (req, res) => {
+//   return controller.getUserProfile(req, res);
+// });
+
+// router.put('/', authenticateToken, updateUserProfileValidation, validate, (req, res) => {
+//   return controller.updateUserProfile(req, res);
+// });
+
+// router.put('/update-by-identifier',
+//   authenticateToken,
+//   authorizeRoles('admin'),
+//   updateUserProfileController.updateUserProfile
+// );
+
+// module.exports = router;
 const express = require("express");
 const router = express.Router();
 const { authAndIdentity } = require('../controller');
 const { authenticateToken } = require('../middleware/authenticateToken');
 const authorizeRoles = require('../middleware/authorizeRoles');
 const validate = require('../middleware/validateRequest');
+const sanitizeInput = require('../middleware/sanitizeInput');
 const { updateUserProfileValidation } = require('../validators/userProfileValidator');
 
 const {
@@ -15,14 +44,22 @@ router.get('/', authenticateToken, (req, res) => {
   return controller.getUserProfile(req, res);
 });
 
-router.put('/', authenticateToken, updateUserProfileValidation, validate, (req, res) => {
-  return controller.updateUserProfile(req, res);
-});
-
-router.put('/update-by-identifier',
+router.put(
+  '/',
   authenticateToken,
-  authorizeRoles('admin'),
-  updateUserProfileController.updateUserProfile
+  sanitizeInput,
+  updateUserProfileValidation,
+  validate,
+  (req, res) => {
+    return controller.updateUserProfile(req, res);
+  }
 );
 
+router.put(
+  '/update-by-identifier',
+  authenticateToken,
+  authorizeRoles('admin'),
+  sanitizeInput,
+  updateUserProfileController.updateUserProfile
+);
 module.exports = router;
